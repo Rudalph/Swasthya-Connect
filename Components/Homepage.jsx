d2cf16fc64e420616fea167bfadd6382c1b6d35d
Rudalph <gonsalvesrudalph@gmail.com>
2024-03-10 22:23:11 +0530

    Homepage draft created

---
+++ Components/Homepage.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navbar from './navbar'
+import Hero from './Hero'
+
+const Homepage = () => {
+  return (
+    <div className="flex h-screen overflow-hidden">
+    {/* Sidebar */}
+    <Navbar />
+
+    {/* Content */}
+    <div className="flex-1 overflow-x-hidden overflow-y-auto">
+      {/* Your Hero Section */}
+      <Hero />
+      {/* Add other components/content as needed */}
+    </div>
+  </div>
+  )
+}
+
+export default Homepage
