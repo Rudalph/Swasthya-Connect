7540ca9bd4780bc055f311ded269d5f426156474
Rudalph <gonsalvesrudalph@gmail.com>
2024-03-21 21:38:19 +0530

    ui updates

--- app/MedicineAlert/page.jsx
+++ app/MedicineAlert/page.jsx
@@ -40,6 +40,7 @@ const ReminderForm = () => {
           <label htmlFor="phone" className="px-3 text-xs font-semibold uppercase text-gray-900">Phone Number</label>
           <input type="tel" id="phone" name="phone" value={phone} onChange={(e) => setPhone(e.target.value)} className="border border-gray-300 px-3 py-2 rounded-md w-full" placeholder="Enter phone number" required />
         </div>
+        <br />
         <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 w-full">Set Reminder</button>
       </form>
       {errorMessage && (
